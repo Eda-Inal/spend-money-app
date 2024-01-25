5dd93f66f4bcc6f27eeac224737da5dd211e94cc
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-25 11:04:51 +0300

    add store and slice

---
+++ src/redux/productsSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productsSlice = createSlice({
+    name : 'products',
+    initialState : {
+items:[
+    {id : "1",}
+]
+    },
+    reducers : {
+
+    }
+})
+export default productsSlice.reducer;
