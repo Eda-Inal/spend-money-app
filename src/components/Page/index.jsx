59ad67ec8112bc2417a9d24892430780d395a4e0
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-27 14:03:27 +0300

    add incrementByAmount function

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import "./styles.css"
-import { buyBtn,sellBtn,incrementByAmount } from '../../redux/productsSlice';
+import { buyBtn, sellBtn, incrementByAmount } from '../../redux/productsSlice';
 
 function Page() {
   const items = useSelector(state => state.products.items);
-  const [inputValue,setInputValue] = useState(0)
-  
-  
-  
+  const [inputValues, setInputValues] = useState({})
+
+
+
   const dispatch = useDispatch();
 
-  const handleBuy =(item) => {
-      dispatch(buyBtn({id:item.id,price:item.price}));
+  const handleBuy = (item) => {
+    dispatch(buyBtn({ id: item.id, price: item.price }));
+
+
   }
-  const handleSell =(item) => {
-    dispatch(sellBtn({id:item.id,price:item.price}));
-}
+  const handleSell = (item) => {
+    dispatch(sellBtn({ id: item.id, price: item.price }));
+
+  }
 
 
-  
-  
+
   return (
     <div className='products-container'>
       <div className='product'>
@@ -28,15 +30,22 @@ function Page() {
         {
           items.map((item) => (
             <div key={item.id} className='card'>
-{console.log(item.click)}
+              {console.log(item.click)}
               <img className='img' src={item.image} alt={item.name} />
-              <div style={{fontSize:22,fontWeight:700}}>{item.name}</div>
-              <div style={{color :"#24c486", fontSize:20,fontWeight:500}}>${item.price}</div>
-              
+              <div style={{ fontSize: 22, fontWeight: 700 }}>{item.name}</div>
+              <div style={{ color: "#24c486", fontSize: 20, fontWeight: 500 }}>${item.price}</div>
+
               <div className='btn-card'>
-                <button className= {`btn-sell ${item.click >0? "bg-sell2" : "bg-sell1"}`} onClick={() =>handleSell(item)}>Sell</button>
-                <input type="number" value={inputValue}   />
-                <button className='btn-buy' onClick={()=>handleBuy(item)}>Buy</button>
+                <button className={`btn-sell ${item.click > 0 ? "bg-sell2" : "bg-sell1"}`} onClick={() => handleSell(item)}>Sell</button>
+                <input
+                  type="number"
+                  value={item.click}
+                  onChange={(e) => {
+                    dispatch(incrementByAmount({ id: item.id, amount: parseInt(e.target.value) }))
+                  }}
+                />
+
+                <button className='btn-buy' onClick={() => handleBuy(item)}>Buy</button>
               </div>
 
             </div>
--- src/redux/productsSlice.jsx
+++ src/redux/productsSlice.jsx
@@ -2,37 +2,43 @@ import { createSlice } from "@reduxjs/toolkit";
 import data from "../data/data"
 
 export const productsSlice = createSlice({
-    name : 'products',
-    initialState : {
-items:data,
-price : 100000000000,
+    name: 'products',
+    initialState: {
+        items: data,
+        price: 100000000000,
 
     },
-    reducers : {
-buyBtn : (state,action) =>{
-    const {id,price} =action.payload
-    const selectedItem = state.items.find((item) => item.id===id);
-    if(selectedItem && state.price >= price){
-        selectedItem.click +=1;
-        selectedItem.amount += price
-        state.price -=price
-    }
-   
-},
-sellBtn : (state,action) => {
-    const {id,price} =action.payload;
-    const selectedItem = state.items.find((item) => item.id===id);
-if(selectedItem && selectedItem.click >0){
-    state.price +=price;
-    selectedItem.amount -= price
-    selectedItem.click -=1;
-} 
-    },
-    incrementByAmount : (state,action) => {
-        
-        
-    }
+    reducers: {
+        buyBtn: (state, action) => {
+            const { id, price } = action.payload
+            const selectedItem = state.items.find((item) => item.id === id);
+            if (selectedItem && state.price >= price) {
+                selectedItem.click += 1;
+                selectedItem.amount += price
+                state.price -= price
+            }
+
+        },
+        sellBtn: (state, action) => {
+            const { id, price } = action.payload;
+            const selectedItem = state.items.find((item) => item.id === id);
+            if (selectedItem && selectedItem.click > 0) {
+                state.price += price;
+                selectedItem.amount -= price
+                selectedItem.click -= 1;
+            }
+        },
+        incrementByAmount: (state, action) => {
+            const { id, amount } = action.payload;
+
+            const selectedItem = state.items.find((item) => item.id === id);
+            
+            if (selectedItem) {
+                selectedItem.click = amount
+            }
+        }
+
     }
 });
-export const {buyBtn,sellBtn,incrementByAmount} =productsSlice.actions;
+export const { buyBtn, sellBtn, incrementByAmount } = productsSlice.actions;
 export default productsSlice.reducer;
